d64edf00e27df4cc3257aeb8f64202bff7de48da
buildcloud-nativeapp <peiqiangw@googlemail.com>
2020-07-01 09:56:36 +0100

    Fix minor js warning

--- frontend/react-front-end/src/components/weekplans.js
+++ frontend/react-front-end/src/components/weekplans.js
@@ -5,12 +5,12 @@ const WeekPlans = ({ weekPlans }) => {
     <div>
       <center><h1>Week Plans</h1></center>
       {weekPlans.map((weekPlan) => (
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">{weekPlan.title}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">{(new Date(weekPlan.startDate)).toISOString().split('T')[0]}</h6>
-            <p class="card-text">{weekPlan.goals}</p>
-            <p class="card-text">{weekPlan.tasks}</p>
+        <div className="card" key={weekPlan.id}>
+          <div className="card-body">
+            <h5 className="card-title">{weekPlan.title}</h5>
+            <h6 className="card-subtitle mb-2 text-muted">{(new Date(weekPlan.startDate)).toISOString().split('T')[0]}</h6>
+            <p className="card-text">{weekPlan.goals}</p>
+            <p className="card-text">{weekPlan.tasks}</p>
           </div>
         </div>
       ))}
